a727cb57bb94cd12d373d7a5d4381c2236df9e96
KharinovDmitry <web-giorno@mail.ru>
2023-10-30 21:43:29 +0500

    done

---
+++ Core/Trie/Trie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core.Trie
+{
+    public class Trie
+    {
+        private class Node
+        {
+            public Node[] children;
+            public bool isEnd = false;
+            public Node()
+            {
+                children = new Node[26];
+                isEnd = false;
+            }
+        }
+
+        private Node rootNode = null;
+
+        public Trie()
+        {
+            rootNode = new Node();
+        }
+
+        public void Insert(string word)
+        {
+            Node currNode = rootNode;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (currNode.children[word[i] - 'a'] == null)
+                {
+                    currNode.children[word[i] - 'a'] = new Node();
+                }
+                currNode = currNode.children[word[i] - 'a'];
+            }
+            currNode.isEnd = true;
+        }
+
+        public bool Search(string word)
+        {
+            Node currNode = rootNode;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (currNode.children[word[i] - 'a'] == null)
+                {
+                    return false;
+                }
+                currNode = currNode.children[word[i] - 'a'];
+            }
+            return currNode.isEnd;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            Node currNode = rootNode;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                if (currNode.children[prefix[i] - 'a'] == null)
+                {
+                    return false;
+                }
+                currNode = currNode.children[prefix[i] - 'a'];
+            }
+            return true;
+        }
+
+    }
+}
